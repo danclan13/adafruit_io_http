f3f4534dd48a1e13bae4bf7ab35c534785c207f4
danclan13 <40115635+danclan13@users.noreply.github.com>
2020-07-23 15:47:42 +0000

    Changed something

--- src/ada_io_http.rs
+++ src/ada_io_http.rs
@@ -11,13 +11,13 @@ pub struct AdaClient {
 }
 
 impl AdaClient {
-    pub fn set(&mut self, n1:String, n2:String){
+    pub fn set(n1:String, n2:String){
         self.ada_io_username = n1;
         self.ada_io_key = n2;
     }
 
 
-    pub fn post(&mut self, n3:String, data:String){
+    pub fn post(n3:String, data:String){
         let ada_io_feedkey:String = n3;
         let url:String = format!("https://io.adafruit.com/api/v2/{:}/feeds/{:}/data", 
         self.ada_io_username, ada_io_feedkey);
